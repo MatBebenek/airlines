76ebc6529f45651de70d68c6738899ec823b3361
MatBebenek <mateusz.bebenek2@gmail.com>
2022-06-28 19:10:06 +0200

    tests improvenment

--- src/test/java/com/home365/airlines/it/controller/AirlineControllerIt.java
+++ src/test/java/com/home365/airlines/it/controller/AirlineControllerIt.java
@@ -36,41 +36,6 @@ public class AirlineControllerIt extends IntegrationTestsCommonBehavior {
         deleteAllData();
     }
 
-    @Test
-    void createAirline() throws Exception {
-        // given
-        Destination destination = destinationRepository.saveAndFlush(aDestination());
-        Airline airlineToAdd = anAirline(destination);
-
-        // when
-        this.mockMvc.perform(
-                post("/create-airline")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(asJsonString(Map.of(
-                                "airlineName", airlineToAdd.getAirlineName(),
-                                "budget", airlineToAdd.getBudget(),
-                                "homeBase", airlineToAdd.getHomeBase()
-                        ))))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-                .andExpect(jsonPath("$.id", not(blankString())))
-                .andExpect(jsonPath("$.name", equalTo(airlineToAdd.getAirlineName())))
-                .andExpect(jsonPath("$.budget").value(is(airlineToAdd.getBudget()), BigDecimal.class))
-                .andExpect(jsonPath("$.homeBase").value(is(airlineToAdd.getHomeBase()), BigDecimal.class))
-                .andExpect(jsonPath("$.createdAt", not(blankString())))
-                .andDo(print());
-
-        // and then
-        List<Airline> airlines = Lists.newArrayList(airlineRepository.findAll());
-        assertThat(airlines).hasSize(1);
-        assertThat(airlines).allSatisfy(airline -> {
-            assertThat(airline.getId()).isGreaterThan(0);
-            assertThat(airline.getAirlineName()).isEqualTo(airlineToAdd.getAirlineName());
-            assertThat(airline.getBudget()).isEqualTo(airlineToAdd.getBudget());
-            assertThat(airline.getHomeBase()).isEqualTo(airlineToAdd.getHomeBase());
-            assertThat(airline.getCreatedAt()).isNotNull();
-        });
-    }
 
     // todo: add tests for
     // create location without name, latitude or longitude
